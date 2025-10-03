ac4912305c3d82f9ed3c9b1e3c1becde3f352ae0
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2025-10-03 21:12:23 +0300

    CSharp ile toplama işlemi

---
+++ CSharp Solve Me First/Program.cs
@@ -0,0 +1,24 @@
+﻿while (true)
+{
+    Console.Write("Lütfen aralarına virgül gelecek şekilde iki sayı giriniz ve onların toplamlarını gör.");
+    string? strNumbers = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(strNumbers))
+    {
+        Console.WriteLine("Lütfen boş geçmeyiniz!!");
+    }
+
+    string[] strNumbersArray = strNumbers.Split(",");
+
+    int[] intNumbersArray = Array.ConvertAll(strNumbersArray, int.Parse);
+
+    int result = 0;
+
+    for (int i = 0; i < intNumbersArray.Length; i++)
+    {   
+        result += intNumbersArray[i];
+    }
+
+    Console.WriteLine($"Toplama işleminin sonucu: {result}");
+}
+
