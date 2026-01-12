0024012211be686ec4a67e9061a54d40ca60f66c
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2026-01-12 13:53:39 +0300

    CSHARP ile Find the first number in the sequence that doesn't repeat

---
+++ Find the first number in the sequence that doesn't repeat/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Virgül ile ayırarak rakamlar giriniz.");
+string? strNumbers = Console.ReadLine();
+
+if (string.IsNullOrEmpty(strNumbers))
+{
+    Console.WriteLine("Boş geçmeyiniz!");
+    return;
+}
+
+string[] strArrayNumbers = strNumbers.Split(",");
+int[] intArraysNumbers = Array.ConvertAll(strArrayNumbers,int.Parse);
+
+int notRepeat = intArraysNumbers[0];
+
+List<int> repeats = new List<int>();
+
+for (int i = 1; i < intArraysNumbers.Length; i++)
+{
+    if (intArraysNumbers[i] == notRepeat)
+    {
+        repeats.Add(notRepeat);
+    }
+    else
+    {
+        
+        if (!repeats.Contains(notRepeat))
+        {
+            break; 
+        }
+
+        
+        notRepeat = intArraysNumbers[i];
+    }
+    
+}
+
+Console.WriteLine("Tekrar etmeyen sayı: " + notRepeat);
