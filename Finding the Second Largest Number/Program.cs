2646f100d0884ae34cf4b6f57a2f793938a2801e
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2026-01-27 21:04:15 +0300

    CSHARP ile Finding the Second Largest Number

---
+++ Finding the Second Largest Number/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Virgül ile sayılar giriniz");
+string? strNumbers = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(strNumbers))
+{
+    Console.WriteLine("Boş geçmeyiniz!");
+}
+string[] strArrayNumbers = strNumbers.Split(",");
+int[] intArrayNumbers = Array.ConvertAll(strArrayNumbers,int.Parse);
+int bignumber = intArrayNumbers[0];
+int second = intArrayNumbers[0];
+for (int i = 0; i < intArrayNumbers.Length; i++)
+{
+    if (intArrayNumbers[i] > bignumber)
+    {
+        second = bignumber;
+        bignumber = intArrayNumbers[i];
+    }
+    else if (intArrayNumbers[i] > second && intArrayNumbers[i] != bignumber)
+    {
+        second = intArrayNumbers[i];
+    }
+}
+
+Console.WriteLine("En büyük ikinci sayı: " + second);
