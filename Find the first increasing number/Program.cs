b550ef225cf46a24bd1a78fea4428560070a312f
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2026-02-04 18:00:51 +0300

    CSHARP ile Find the first increasing number

---
+++ Find the first increasing number/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Virgül ile ayırarak liste giriniz.");
+string numbers = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(numbers))
+{
+    Console.WriteLine("Boş geçmeyiniz");
+    return;
+}
+string[] strArray = numbers.Split(",");
+int[] intArray = Array.ConvertAll(strArray,int.Parse);
+for (int i = 1; i < intArray.Length; i++)
+{
+    if (intArray[i] > intArray[i - 1])
+    {
+        Console.WriteLine($"İlk artan sayı: {intArray[i]}");
+        break;
+    }
+}
+
