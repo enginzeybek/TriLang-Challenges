7b2dc9e1176039e119ef2a77344bd87ee991a411
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2026-03-13 17:43:39 +0300

    CSHARP ile Find the longest first word

---
+++ Find the longest first word/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.Unicode;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Lütfen boşluk ile ayrılmış bir cümle giriniz.");
+string sentence = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(sentence))
+{
+    Console.WriteLine("Lütfen boş geçmeyiniz!");
+}
+string[] strSentence = sentence.Split(" ");
+string result = strSentence[0];
+for (int i = 0; i < strSentence.Length; i++)
+{
+    string maxWord = strSentence[i];
+
+    if (maxWord.Length > result.Length)
+    {
+        result = maxWord;
+        
+    }
+}
+
+Console.WriteLine($"En uzun kelime: {result}");
